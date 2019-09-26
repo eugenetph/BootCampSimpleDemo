5a1589b7811bb4adc7dcbfd8115c5dae457011bc
eugene <eugenetph@gmail.com>
2019-09-27 00:46:12 +0800

    changes

--- src/App.js
+++ src/App.js
@@ -5,32 +5,36 @@ import FishSoup from './asset/Han-Kee-Fish-Soup-2.jpg'
 
 //Building Component
 const Building = () => {
-  const grabFood = {
+  const order = {
     foodName: "Fish Soup + Rice ",
     cost: '$7.50',
     recipientName: "Eugene Tan",
-    img: FishSoup
+    img: FishSoup,
+    isPaid: false
   };
-  return <Floor1 grabFood={grabFood} />;
+
+  const [grabFood, setGrabFood] = useState(order)
+
+  return <Floor1 grabFood={grabFood} setGrabFood={setGrabFood} />;
 };
 
 //Floor1 Component
-const Floor1 = ({ grabFood }) => {
-  return <Floor2 grabFood={grabFood} />;
+const Floor1 = ({ grabFood, setGrabFood }) => {
+  return <Floor2 grabFood={grabFood} setGrabFood={setGrabFood} />;
 };
 
 //Floor2 Component
-const Floor2 = ({ grabFood }) => {
-  return <Floor3 grabFood={grabFood} />;
+const Floor2 = ({ grabFood, setGrabFood }) => {
+  return <Floor3 grabFood={grabFood} setGrabFood={setGrabFood} />;
 };
 
 //Floor3 Component
-const Floor3 = ({ grabFood }) => {
-  return <Floor12 grabFood={grabFood} />;
+const Floor3 = ({ grabFood, setGrabFood }) => {
+  return <Floor12 grabFood={grabFood} setGrabFood={setGrabFood} />;
 };
 
 //Floor 7 Component
-const Floor12 = ({ grabFood }) => {
+const Floor12 = ({ grabFood, setGrabFood }) => {
   return (
     <Fragment>
       <h3>Fish Soup Delivered to my house at floor 12</h3>
@@ -43,10 +47,17 @@ const Floor12 = ({ grabFood }) => {
         {grabFood.recipientName}
       </p>
       <p>
-        <strong>Package: </strong>
+        <strong>Cost: </strong>
         {grabFood.cost}
       </p>
-      <img src={grabFood.img} alt='fish soup'/>
+      <p>
+        <strong>isPaid: </strong>
+        {grabFood.isPaid ? 'Paid' : 'Not Paid'}
+      </p>
+      <p>
+        <button onClick={() => setGrabFood({ ...grabFood, isPaid: true })}>Pay now</button>
+      </p>
+      <img src={grabFood.img} alt='fish soup' />
     </Fragment>
   );
 };
@@ -57,7 +68,7 @@ function App() {
     <div className="App">
       <h1>Simple Context API Demo using React Hooks</h1>
       {!isStart && <><p><strong>I love fish soup and craving for it so I order through grabFood app. Feel excited while waiting for it!</strong></p>
-       <button onClick={() => setisStart(true)}>Start Deliver</button> </>}
+        <button onClick={() => setisStart(true)}>Start Deliver</button> </>}
       {isStart && <Building />}
     </div>
   );
