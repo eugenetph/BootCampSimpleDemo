eafa0bc6ea6bc35b1cbfe1e2ad56be9bf502e06a
eugene <eugenetph@gmail.com>
2019-09-26 23:35:10 +0800

    deliver fish soup first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
+
+//Building Component
+const Building = () => {
+  const grabFood = {
+    foodName: "Fish Soup + Rice",
+    cost: '$7.50',
+    recipientName: "Eugene Tan"
+  };
+  return <Floor1 grabFood={grabFood} />;
+};
+
+//Floor1 Component
+const Floor1 = ({ grabFood }) => {
+  return <Floor2 grabFood={grabFood} />;
+};
+
+//Floor2 Component
+const Floor2 = ({ grabFood }) => {
+  return <Floor3 grabFood={grabFood} />;
+};
+
+//Floor3 Component
+const Floor3 = ({ grabFood }) => {
+  return <Floor7 grabFood={grabFood} />;
+};
+
+//Floor 7 Component
+const Floor7 = ({ grabFood }) => {
+  return (
+    <Fragment>
+      <h3>Welcome to Floor 7</h3>
+      <p>
+        <strong>Food Name: </strong>
+        {grabFood.foodName}
+      </p>
+      <p>
+        <strong>Recipient Name: </strong>
+        {grabFood.recipientName}
+      </p>
+      <p>
+        <strong>Package: </strong>
+        {grabFood.cost}
+      </p>
+    </Fragment>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple Context API Demo using Hook(useState)</h1>
+      <Building />
     </div>
   );
 }
